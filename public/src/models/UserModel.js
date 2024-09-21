7b1dbafa264027a747b0d2e39ca90c78bea4e432
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2024-09-21 20:27:01 -0300

    cpf unique

--- public/src/models/UserModel.js
+++ public/src/models/UserModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true }, // Identificador do usuário
     name: { type: String, required: true },
-    cpf: { type: String, required: true }
+    cpf: { type: String, required: true, unique: true }
     
 });
 
