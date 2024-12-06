079d7979bc70c8dedc1ed02f86e78ad4c0cf4c9f
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2024-12-06 20:30:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Conectar ao MongoDB (login e produtos)
-mongoose.connect('mongodb://localhost:27017/primegaming', {});
+mongoose.connect('mongodb://127.0.0.1:27017/primegaming', {
+});
 
 // Console conexão com MongoDB
 const db = mongoose.connection;
